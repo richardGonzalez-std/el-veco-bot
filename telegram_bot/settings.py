ac9ce8e3e0acca6494e7aacf2f64e68a1c81cebc
Richard Gonzalez <zambranoa998@gmail.com>
2025-07-30 11:26:36 -0400

    update templates

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -108,6 +108,21 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 # Internationalization
